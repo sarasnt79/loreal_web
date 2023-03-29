10cfe2afdff0a1ba548bd96fe71c001c9480cc53
sarasnt <sarasanati24@gmail.com>
2023-03-29 20:19:49 +0430

    seconde

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,54 @@
+const backbtn=document.querySelectorAll('.back');
+const ProductArray=document.querySelectorAll('.Product');
+const containerArray=document.querySelectorAll('.container');
+const bg=document.querySelector('.first');
+const upFirst=document.querySelector('.index');
+let index=0;
 
-let  down=document.querySelector(".downPart");
-let  up=document.querySelector(".upPart");
+var counter=1;
 
-let num=0;
+
+
+
+
+ProductArray.forEach((e ,index) =>{
+    e.addEventListener('click' , ()=>{
+
+         containerArray[index].classList.add('show');
+         
+    })
+})
+
+backbtn.forEach((e ,index)=>{
+   e.addEventListener('click' , ()=>{
+      containerArray[index].classList.remove('show');
+   })
+})
+
+window.addEventListener('scroll' , ()=>{
+   bg.style.opacity=1-window.pageYOffset/600;
+})
 
 window.onscroll=function(){
-
-   num=document.documentElement.scrollTop;
+   if(document.documentElement.scrollTop>200){
+      upFirst.style.display='inline-block';
+   }
+   else{
+      upFirst.style.display='none';
+   }
 }
 
-
-down.addEventListener('click' , () =>{
-window.scrollTo(0,num+1250)
-    
-})
-
-up.addEventListener('click' , () =>{
-    window.scrollTo(0,0)
+upFirst.addEventListener('click' , ()=>{
+  window.scrollTo(0 ,0);
 })
 
 
-// // function fil(){
-// //     window.onscroll=function(){
-// //         num =document.documentElement.scrollTo;
-// //    }
- 
-// //   alert(num)
-// // }
+
+setInterval(()=>{
+   document.querySelector('#radio'+counter).checked=true;
+   counter++;
+   if(counter>4){
+      counter=1;
+   }
+
+} ,3000)
